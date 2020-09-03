6301e61f5358158a83d35828bd90809ae9e97d8b
Teemu Mäkelä <teemu_makela@outlook.com>
2020-09-03 11:15:31 +0300

    Exercises 2.1 - 2.5 done

---
+++ osa2/kurssitiedot/src/components/course.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Header = ({course}) => {
+  return (
+    <>
+      <h1>{course.name}</h1>
+    </>
+  )
+}
+  
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+  
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+  
+const Total = ({parts}) => {
+  const total = parts
+                .map(part => part.exercises)
+                .reduce((sum, val) => sum + val, 0)
+  return (
+    <>
+      <b>total of {total} exercises</b>
+    </>
+  )
+}
+  
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
